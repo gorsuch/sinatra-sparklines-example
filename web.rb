068cc779b94c98e3ac0fafb747d4a6f2345d5282
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-02-08 15:21:45 -0600

    simulate rolling data

--- web.rb
+++ web.rb
@@ -2,6 +2,13 @@ require 'json'
 require 'sinatra'
 
 helpers do
+  # simulate rolling data, updates every time it is called
+  def data
+    @@data ||= generate_data
+    @@data.shift
+    @@data.push rand(100)
+  end
+  
   def generate_data
     Array.new(50).map { rand(100) }
   end
@@ -13,5 +20,5 @@ end
 
 get('/json_endpoint') do
   content_type :json
-  generate_data.to_json
+  data.to_json
 end
